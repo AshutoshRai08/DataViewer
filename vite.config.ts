a7a466b56ae708337b832eea3525463965ac7cd6
AshutoshRai08 <rai.ashu0tosh11@gmail.com>
2025-03-09 03:47:57 +0530

    VIte changes for reloading

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  base: "./", 
   build: {
     chunkSizeWarningLimit: 1000, // Increase the warning limit (optional)
     rollupOptions: {
