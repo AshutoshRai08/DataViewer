b1ad46ea8f9aa62b5356f095dcd8757d4ec39428
AshutoshRai08 <rai.ashu0tosh11@gmail.com>
2025-03-09 03:32:24 +0530

    Second check for CI/CD

--- src/components/TopNavBar/TopNav.tsx
+++ src/components/TopNavBar/TopNav.tsx
@@ -21,7 +21,7 @@ const TopNav: React.FC = () => {
 
         {/* Centered Title */}
         <Typography variant="h6" sx={{ flexGrow: 1, textAlign: "center" }}>
-          Data Viewer 123
+          Data Viewer 1234567
         </Typography>
 
         {/* Logout Button */}
